45b078c271884e3459faad27f99648dd32092759
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-08-16 01:22:33 +0900

    0802_1:bindで取れる主なイベント

--- Chapter08/list0802_1.py
+++ Chapter08/list0802_1.py
@@ -9,3 +9,12 @@ root = tkinter.Tk()
 root.title("キーコードを取得")
 root.bind("<KeyPress>", key_down)
 root.mainloop()
+
+"""
+bind()で取れる主なイベント
+<KeyPress>      キーを押した
+<Key>           同上
+<Motion>        マウスポインタを動かした
+<ButtonPress>   マウスボタンをクリックした
+<Button>        同上
+"""
