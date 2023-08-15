4bd3ce736001d302ccca982e55dc5c27f31b99bb
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-08-16 01:34:10 +0900

    0803_1:主なキーコードを加筆

--- Chapter08/list0803_1.py
+++ Chapter08/list0803_1.py
@@ -15,3 +15,16 @@ label = tkinter.Label(font=("Times New Roman", 80))
 label.pack()
 main_proc()
 root.mainloop()
+
+"""
+主なキーコード(Mac)
+←       8124162
+↑       8320768
+→       8189699
+↓       8255233
+Space   32
+Return  2359309
+A~Z     65~90
+a~z     97~122
+0~9     48~57
+"""
