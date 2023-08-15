b7b44367416aaa6f07dd170e2bec4f7d3caf2ebd
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-08-15 23:52:49 +0900

    0801-1:数字がどんどん増えていく

--- Chapter08/list0801-1.py
+++ Chapter08/list0801-1.py
@@ -1,1 +1,14 @@
     # after()命令を使う
+import tkinter
+tmr = 0
+def count_up():
+    global tmr
+    tmr += 1
+    label["text"] = tmr
+    root.after(1000, count_up)
+
+root = tkinter.Tk()
+label = tkinter.Label(font=("Times New Roman", 80))
+label.pack()
+root.after(1000, count_up)
+root.mainloop()
