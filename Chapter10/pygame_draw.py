f66a0e5bee6bc58ef3a1a52c637c1be2aa34814d
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-08-20 01:25:01 +0900

    1000_draw:幾何描画命令

--- Chapter10/pygame_draw.py
+++ Chapter10/pygame_draw.py
@@ -24,6 +24,37 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        
         screen.fill(BLACK)
     
-    
+        pygame.draw.line(screen, RED, [0, 0], [100, 200], 10)
+        pygame.draw.lines(screen, BLUE, False, [[50, 300], [150, 400], [50, 500]])
+        
+        pygame.draw.rect(screen, RED, [200,50,120,80])
+        pygame.draw.rect(screen, GREEN, [200,200,60,180], 5)
+        pygame.draw.polygon(screen, BLUE, [[250,400], [200,500], [300,500]], 10)
+        
+        pygame.draw.circle(screen, GOLD, [400,100], 60)
+        pygame.draw.ellipse(screen, SILVER, [400-80,300-40,160,80])
+        pygame.draw.ellipse(screen, COPPER, [400-40,500-80,80,160], 20)
+
+        ang = math.pi*tmr/36
+        pygame.draw.arc(screen, BLUE, [600-100,300-200,200,400], 0, math.pi*2, 8)
+        pygame.draw.arc(screen, WHITE, [600-100,300-200,200,400], ang, ang+math.pi/2, 8)
+        
+        pygame.display.update()
+        clock.tick(10)
+
+if __name__ == '__main__':
+    main()
+
+"""     ----図形の描画命令----
+線      line
+連続線  lines
+矩形    rect
+多角形  polygon
+円      circle
+楕円    ellipse
+円弧    arc
+※   角度は弧度法(radian)で指定
+"""
