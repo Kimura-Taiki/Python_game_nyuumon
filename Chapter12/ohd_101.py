895cc08e4fdf3714b785ea0a137d99033693a6f9
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-12 22:25:18 +0900

    12x36:方向キー移動をpartial化

--- Chapter12/ohd_101.py
+++ Chapter12/ohd_101.py
@@ -147,24 +147,38 @@ def move_player(key): # 主人公の移動
     # 方向キーで上下左右に移動
     x = pl_x
     y = pl_y
-    if key[K_UP] == 1:
-        pl_d = 0
-        if dungeon[pl_y-1][pl_x] != 9:
-            pl_y -= 1
-    if key[K_DOWN] == 1:
-        pl_d = 1
-        if dungeon[pl_y+1][pl_x] != 9:
-            pl_y += 1
-    if key[K_LEFT] == 1:
-        pl_d = 2
-        if dungeon[pl_y][pl_x-1] != 9:
-            pl_x -= 1
-    if key[K_RIGHT] == 1:
-        pl_d = 3
-        if dungeon[pl_y][pl_x+1] != 9:
-            pl_x += 1
+    def move(bool, k_code, dir, dx, dy):
+        global pl_x, pl_y, pl_d
+        if key[k_code] != 1: return bool or False
+        pl_d = dir
+        if dungeon[pl_y+dy][pl_x+dx] == 9: return bool or False
+        pl_x += dx
+        pl_y += dy
+        return True
+    is_move = pipeline_each(False,
+                            [partial(move, k_code=K_UP,   dir=0, dx= 0, dy=-1),
+                             partial(move, k_code=K_DOWN, dir=1, dx= 0, dy= 1),
+                             partial(move, k_code=K_LEFT, dir=2, dx=-1, dy= 0),
+                             partial(move, k_code=K_RIGHT,dir=3, dx= 1, dy= 0)])
+    # if key[K_UP] == 1:
+    #     pl_d = 0
+    #     if dungeon[pl_y-1][pl_x] != 9:
+    #         pl_y -= 1
+    # if key[K_DOWN] == 1:
+    #     pl_d = 1
+    #     if dungeon[pl_y+1][pl_x] != 9:
+    #         pl_y += 1
+    # if key[K_LEFT] == 1:
+    #     pl_d = 2
+    #     if dungeon[pl_y][pl_x-1] != 9:
+    #         pl_x -= 1
+    # if key[K_RIGHT] == 1:
+    #     pl_d = 3
+    #     if dungeon[pl_y][pl_x+1] != 9:
+    #         pl_x += 1
     pl_a = pl_d*2
-    if pl_x != x or pl_y != y: # 移動したら食料の量と体力を計算
+    # if pl_x != x or pl_y != y: # 移動したら食料の量と体力を計算
+    if is_move == True: # 移動したら食料の量と体力を計算
         pl_a += tmr%2 # 移動したら足踏みのアニメーション
         if food > 0:
             food -= 1
@@ -177,6 +191,73 @@ def move_player(key): # 主人公の移動
                 pygame.mixer.music.stop()
                 idx = Idx.GAME_OVER
                 tmr = 0
+
+# def move_player(key): # 主人公の移動
+#     global idx, tmr, pl_x, pl_y, pl_d, pl_a, pl_life, food, potion, blazegem, treasure
+
+#     if dungeon[pl_y][pl_x] == 1: # 宝箱に載った
+#         dungeon[pl_y][pl_x] = 0
+#         treasure = random.choice([0,0,0,1,1,1,1,1,1,2])
+#         if treasure == 0:
+#             potion += 1
+#         if treasure == 1:
+#             blazegem += 1
+#         if treasure == 2:
+#             food = int(food/2)
+#         idx = Idx.ON_ITEM
+#         tmr = 0
+#         return
+#     if dungeon[pl_y][pl_x] == 2: # 繭に載った
+#         dungeon[pl_y][pl_x] = 0
+#         r = random.randint(0, 99)
+#         if r < 40: # 食料
+#             treasure = random.choice([3,3,3,4])
+#             if treasure == 3: food += 20
+#             if treasure == 4: food += 100
+#             idx = Idx.ON_ITEM
+#             tmr = 0
+#         else: # 敵出現
+#             idx = Idx.ON_ENEMY
+#             tmr = 0
+#             return
+#     if dungeon[pl_y][pl_x] == 3: # 階段に載った
+#         idx = Idx.ON_STAIRS
+#         tmr = 0
+#         return
+    
+#     # 方向キーで上下左右に移動
+#     x = pl_x
+#     y = pl_y
+#     if key[K_UP] == 1:
+#         pl_d = 0
+#         if dungeon[pl_y-1][pl_x] != 9:
+#             pl_y -= 1
+#     if key[K_DOWN] == 1:
+#         pl_d = 1
+#         if dungeon[pl_y+1][pl_x] != 9:
+#             pl_y += 1
+#     if key[K_LEFT] == 1:
+#         pl_d = 2
+#         if dungeon[pl_y][pl_x-1] != 9:
+#             pl_x -= 1
+#     if key[K_RIGHT] == 1:
+#         pl_d = 3
+#         if dungeon[pl_y][pl_x+1] != 9:
+#             pl_x += 1
+#     pl_a = pl_d*2
+#     if pl_x != x or pl_y != y: # 移動したら食料の量と体力を計算
+#         pl_a += tmr%2 # 移動したら足踏みのアニメーション
+#         if food > 0:
+#             food -= 1
+#             if pl_life < pl_lifemax:
+#                 pl_life += 1
+#         else:
+#             pl_life -= 5
+#             if pl_life <= 0:
+#                 pl_life = 0
+#                 pygame.mixer.music.stop()
+#                 idx = Idx.GAME_OVER
+#                 tmr = 0
 
 def draw_text(bg, txt, x, y, fnt, col): # 影付き文字の表示
     sur = fnt.render(txt, True, BLACK)
