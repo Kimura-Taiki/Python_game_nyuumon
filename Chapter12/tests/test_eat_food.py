efe1dfab75d068fe9f04d925dfca6aced711fe6d
Kimura-Taiki <88486709+Kimura-Taiki@users.noreply.github.com>
2023-09-20 00:57:45 +0900

    12x78:test_eat_foodが完成

--- Chapter12/tests/test_eat_food.py
+++ Chapter12/tests/test_eat_food.py
@@ -1,13 +1,29 @@
 import sys
 import os
+import pytest
 
 # これ結構重要じゃない？
 if __name__ == '__main__': sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+from mod.scenes.field_wfi import eat_food
 # print(sys.path)
 
-def test_hoge():
-    assert True
+def walk(n):
+    food = 10
+    pl_life = 10
+    pl_lifemax = 12
+    for i in range(n):
+        food, pl_life = eat_food(food=food, pl_life=pl_life, pl_lifemax=pl_lifemax)
+    return food, pl_life
 
-def test_fuga():
-    assert False
+@pytest.mark.parametrize(('n', 'food', 'pl_life'), [
+    ( 0, 10, 10),
+    ( 2,  8, 12),
+    ( 6,  4, 12),
+    (10,  0, 12),
+    (12,  0,  2),
+    (13,  0,  0),
+    (20,  0,  0)
+])
+def test_eat_food(n, food, pl_life):
+    assert walk(n) == (food, pl_life)
